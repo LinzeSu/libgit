b532fd3f4fe05d57292cb6864127bfebee011dce
lize su <lingzhe.sun@hirain.com>
2021-07-23 18:28:59 +0800

    still conflicts test

--- test.rb
+++ test.rb
@@ -1,3 +1,3 @@
 just changed something 20210723
-made more change on testing branch
+made more change on testing branch and test on the second line to see conflicts
 did a manual modification when merging error from testing branch to master occurs
