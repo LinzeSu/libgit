0fc34ac5b47d7b7b8b7b3ac51c60e4645f87276e
lize su <lingzhe.sun@hirain.com>
2021-07-26 09:52:26 +0800

    new conflicts test

--- test.rb
+++ test.rb
@@ -1,3 +1,18 @@
 just changed something 20210723 and test on conflicts
 made more change on testing branch
 did a manual modification when merging error from testing branch to master occurs
+
+
+
+
+
+hah
+This may be a bit confusing, so let’s look at an example. Let’s say you have a Git server on your network at git.ourcompany.com. If you clone from this, Git’s clone command automatically names it origin for you, pulls down all its data, creates a pointer to where its master branch is, and names it origin/master locally. Git also gives you your own local master branch starting at the same place as origin’s master branch, so you have something to work from.
+
+
+
+
+
+
+
+let this be the line that could be changed.
